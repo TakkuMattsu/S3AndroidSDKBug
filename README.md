8c24fcdec686b286d0bc3f43e3f4c0c2851a5c5d
TakkuMattsu <takku.mattsu@gmail.com>
2017-10-26 22:45:02 +0900

    s3のサンプル

---
+++ README.md
@@ -0,0 +1,3 @@
+# 以下の記事を試してみただけ
+
+https://qiita.com/niusounds/items/3d9bb0429716107c3c8c
--- app/src/main/java/com/example/t_matsumura/myapplication/MainActivity.java
+++ app/src/main/java/com/example/t_matsumura/myapplication/MainActivity.java
@@ -2,12 +2,60 @@ package com.example.t_matsumura.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String IDENTITY_POOL_ID = "ここにプールID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                IDENTITY_POOL_ID, // 実際のIdentity poolに置き換える
+                Regions.AP_NORTHEAST_1);
+
+        AmazonS3Client s3 = new AmazonS3Client(credentialsProvider);
+        TransferUtility transferUtility = new TransferUtility(s3, getApplicationContext());
+
+        String bucketName = "バケット名";
+        String fileName = "ここにコピーパス";
+        final File file = new File(getFilesDir(), fileName);
+
+        TransferObserver observer = transferUtility.download(bucketName, fileName, file);
+        observer.setTransferListener(new TransferListener() {
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                switch (state) {
+                    case COMPLETED:
+                        // ダウンロード完了時
+                        Log.d("s3Test", file.getAbsolutePath());
+                        break;
+                    case FAILED:
+                        break;
+                }
+            }
+
+            @Override
+            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+            }
+
+            @Override
+            public void onError(int id, Exception ex) {
+                Log.e("s3Test",ex.getLocalizedMessage());
+            }
+        });
     }
 }
